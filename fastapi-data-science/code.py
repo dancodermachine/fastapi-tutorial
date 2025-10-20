ce610cf963b81299530e84a059178a70a0c77807
Daniel <60199577+dancodermachine@users.noreply.github.com>
2025-10-20 19:19:52 +0100

    chap03 tutorial the request body

--- fastapi-data-science/code.py
+++ fastapi-data-science/code.py
@@ -1,19 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from enum import Enum
+from pydantic import BaseModel
+
+
+
+# @app.get("/")
+# async def hello_world():
+#     """
+#     1. We define GET endpoint at the root path.
+#     2. It produces a proper HTTP response with a JSON payload.
+#     3. A decorator is a syntatic sugar that allows you to wrap a function or class with common logic without compromising readability.
+#     4. FastAPI exposes one decorator per HTTP method to add new routes to the application.
+#     5. FastAPI exposes an Asynchronous Server Gateway Interface (ASGI) - compatible application.
+#     """
+#     return {"hello": "world"}
+
+class User(BaseModel):
+    name: str
+    age: int
 
 app = FastAPI()
 
-@app.get("/")
-async def hello_world():
-    """
-    1. We define GET endpoint at the root path.
-    2. It produces a proper HTTP response with a JSON payload.
-    3. A decorator is a syntatic sugar that allows you to wrap a function or class with common logic without compromising readability.
-    4. FastAPI exposes one decorator per HTTP method to add new routes to the application.
-    5. FastAPI exposes an Asynchronous Server Gateway Interface (ASGI) - compatible application.
-    """
-    return {"hello": "world"}
-
-@app.get("/users")
-async def get_user(page: int = 1, size: int = 10):
-    return {"page": page, "size": size}
+@app.post("/users")
+async def create_user(user: User, priority: int = Body(..., ge=1, le=3)):
+    return {"user": user, "priority": priority}
